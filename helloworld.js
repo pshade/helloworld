a98e82a4bc9c454a2eb63a654aa157e1c1b57218
pshade <45977721+pshade@users.noreply.github.com>
2019-06-10 20:42:32 -0400

    changed content-type to html

--- helloworld.js
+++ helloworld.js
@@ -5,7 +5,7 @@ http.createServer(function (request, response) {
    // Send the HTTP header
    // HTTP Status: 200 : OK
    // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'})
+   response.writeHead(200, {'Content-Type': 'text/html'})
 
    // Send the response body as "Hello World"
    response.end('Hello World Application - Deployed with AWS CodeDeploy/CodePipeline\n')
